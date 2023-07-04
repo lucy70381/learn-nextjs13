31f18b967ed739a62a4f4e2389ff261aa7132554
Lucy <43696299+lucy70381@users.noreply.github.com>
2023-07-04 11:30:27 +0800

    feat: adjust Modal

--- app/parallel_routes/@authModal/login/page.tsx
+++ app/parallel_routes/@authModal/login/page.tsx
@@ -1,6 +1,14 @@
+"use client";
 import Modal from "@/components/modal";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
   // use router.back() or Link to dimiss the modal
-  return <Modal />;
+  return (
+    <Modal>
+      <div>Login Modal</div>
+      <button onClick={router.back}>Close Modal</button>
+    </Modal>
+  );
 }
--- components/modal.tsx
+++ components/modal.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 
-export default function Modal() {
+export default function Modal({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
   return (
@@ -12,48 +12,8 @@ export default function Modal() {
         onClick={() => router.back()}
       ></div>
       <div className="flex min-h-screen items-center px-4 py-8">
-        <div className="relative mx-auto w-full max-w-lg rounded-md bg-white p-4 shadow-lg">
-          <div className="mt-3">
-            <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 text-green-600"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <div className="mt-2 text-center">
-              <h4 className="text-lg font-medium text-gray-800">
-                Successfully accepted!
-              </h4>
-              <p className="mt-2 text-[15px] leading-relaxed text-gray-500">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc
-                eget lorem dolor sed viverra ipsum nunc. Consequat id porta nibh
-                venenatis.
-              </p>
-            </div>
-          </div>
-          {/* <div className="mt-3 items-center gap-2 sm:flex">
-            <button
-              className="mt-2 w-full flex-1 rounded-md bg-indigo-600 p-2.5 text-white outline-none ring-indigo-600 ring-offset-2 focus:ring-2"
-              onClick={() => setState(false)}
-            >
-              Dashboard
-            </button>
-            <button
-              className="mt-2 w-full flex-1 rounded-md border p-2.5 text-gray-800 outline-none ring-indigo-600 ring-offset-2 focus:ring-2"
-              onClick={() => setState(false)}
-            >
-              Undo
-            </button>
-          </div> */}
+        <div className="relative mx-auto w-full max-w-lg rounded-md bg-white p-4 text-black shadow-lg">
+          {children}
         </div>
       </div>
     </div>
